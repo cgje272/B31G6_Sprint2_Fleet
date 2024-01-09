cddfd49d8986e814a9ac30464302c883865baf5e
Roman Stetsyk <photostetsyk@gmail.com>
2024-01-08 23:02:06 -0500

    ORO Roman Updates

--- src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/OroincDocumentationPage_StepDefinitions.java
+++ src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/OroincDocumentationPage_StepDefinitions.java
@@ -2,6 +2,7 @@ package com.B31G6_Sprint2_Fleet.step_definitions;
 
 import com.B31G6_Sprint2_Fleet.pages.HomePage;
 import com.B31G6_Sprint2_Fleet.pages.LoginPage;
+import com.B31G6_Sprint2_Fleet.utilities.BrowserUtils;
 import com.B31G6_Sprint2_Fleet.utilities.ConfigurationReader;
 import com.B31G6_Sprint2_Fleet.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -30,20 +31,23 @@ public class OroincDocumentationPage_StepDefinitions {
         homePage.oroinclLink.click();
 
     }
-
-
     @Then("new page url should be https:\\/\\/doc.oroinc.com\\/")
-    public void newPageUrlShouldBeHttpsDocOroincCom() {
+    public void new_page_url_should_be_https_doc_oroinc_com() {
+
         for (String each : Driver.getDriver().getWindowHandles()) {
             Driver.getDriver().switchTo().window(each);
-            }
-        String expectedURL="https://doc.oroinc.com/";
-        String actualURL=Driver.getDriver().getCurrentUrl();
-        Assert.assertEquals(expectedURL,actualURL);
+            BrowserUtils.sleep(1);}
+            String expectedURL="https://doc.oroinc.com/";
+            String actualURL=Driver.getDriver().getCurrentUrl();
+            Assert.assertEquals(expectedURL,actualURL);
+        }
 
+
+
+
+      //List<String > windowdHandles= Driver.getDriver().getWindowHandles();
 
     }
-}
 
 
 
