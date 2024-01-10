dd013eb60515e713c468c76e662575f1ee6dde71
Kolesnick1 <kolesnik.valeriy@gmail.com>
2024-01-10 15:54:03 -0800

    Finished the step_def page for US08_AC2 Added some WebElements to CreateCalendarEventPage

--- src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/US08_StepDefs.java
+++ src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/US08_StepDefs.java
@@ -5,12 +5,17 @@ import com.B31G6_Sprint2_Fleet.pages.CreateCalendarEventPage;
 import com.B31G6_Sprint2_Fleet.pages.HomePage;
 import com.B31G6_Sprint2_Fleet.utilities.BrowserUtils;
 import com.B31G6_Sprint2_Fleet.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US08_StepDefs extends HomePage {
     Actions actions = new Actions(Driver.getDriver());
@@ -25,7 +30,7 @@ public class US08_StepDefs extends HomePage {
 
     @When("clicks Calendar Events")
     public void clicks_calendar_events() {
-       actions.moveToElement(this.calendarEvents).click().perform();
+        actions.moveToElement(this.calendarEvents).click().perform();
         BrowserUtils.sleep(5);
     }
 
@@ -41,10 +46,23 @@ public class US08_StepDefs extends HomePage {
 
     @Then("user should see number {int} displayed by default in the Days space")
     public void user_should_see_number_displayed_by_default_in_the_days_space(Integer int1) {
-        String expectedResult=""+int1;
+        String expectedResult = "" + int1;
         JavascriptExecutor jsExecutor = (JavascriptExecutor) Driver.getDriver();
         String actualResult = (String) jsExecutor.executeScript("return arguments[0].value;", createCalendarEventPage.daysInputBox);
+//input box does not get updated with changes, therefore
+        //using JavaScriptExecutor is the only way to get that text
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 
-        Assert.assertEquals(expectedResult,actualResult);
+    @And("user clears the Days space")
+    public void userClearsTheDaysSpace() {
+        createCalendarEventPage.daysInputBox.clear();
+    }
+
+
+    @Then("user should see “This value should not be blank.” error message")
+    public void userShouldSeeThisValueShouldNotBeBlankErrorMessage() {
+        Assert.assertTrue(createCalendarEventPage.thisValueShouldNotBeBlank_ErrorMessage.isDisplayed());
+
     }
 }
--- src/test/resources/features/US08_Repetitive_Calendar_Events.feature
+++ src/test/resources/features/US08_Repetitive_Calendar_Events.feature
@@ -12,4 +12,11 @@ Feature: user should be able to create repetitive calendar events
       And checks the Repeat checkbox
       Then user should see number 1 displayed by default in the Days space
 
+  Scenario: user should see “This value should not be blank.” error message when Days space is empty
+    When user hovers over Activities
+    And clicks Calendar Events
+    And clicks Create Calendar Event
+    And checks the Repeat checkbox
+    And user clears the Days space
+    Then user should see “This value should not be blank.” error message
 
