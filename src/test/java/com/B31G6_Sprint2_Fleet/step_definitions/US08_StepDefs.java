2c6b68de5a793f7d33fa68650e39453b1606a731
Kolesnick1 <kolesnik.valeriy@gmail.com>
2024-01-09 19:34:19 -0800

    Created CalendarEventsPage

--- src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/US08_StepDefs.java
+++ src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/US08_StepDefs.java
@@ -1,24 +1,25 @@
 package com.B31G6_Sprint2_Fleet.step_definitions;
 
+import com.B31G6_Sprint2_Fleet.pages.HomePage;
+import com.B31G6_Sprint2_Fleet.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.interactions.Actions;
 
-public class US08_StepDefs {
-
+public class US08_StepDefs extends HomePage {
+Actions actions= new Actions(Driver.getDriver());
+HomePage homePage;
     @When("user hovers over Activities")
     public void user_hovers_over_activities() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+actions.moveToElement(homePage.activities).perform();
     }
     @When("clicks Calendar Events")
     public void clicks_calendar_events() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+      homePage.calendarEvents.click();
     }
     @When("clicks Create Calendar Event")
     public void clicks_create_calendar_event() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("checks the Repeat checkbox")
     public void checks_the_repeat_checkbox() {
