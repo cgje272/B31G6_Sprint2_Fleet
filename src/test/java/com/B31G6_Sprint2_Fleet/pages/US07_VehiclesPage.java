6b5a55c005510d8241fdd2a98145c9f3d7e97d41
oliveradzuver <dzuvero@gmail.com>
2024-01-14 23:09:11 +0100

    changes

--- src/test/java/com/B31G6_Sprint2_Fleet/pages/US07_VehiclesPage.java
+++ src/test/java/com/B31G6_Sprint2_Fleet/pages/US07_VehiclesPage.java
@@ -1,12 +1,14 @@
 package com.B31G6_Sprint2_Fleet.pages;
 
+import com.B31G6_Sprint2_Fleet.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class US07_VehiclesPage {
 
      public US07_VehiclesPage(){
-
+         PageFactory.initElements(Driver.getDriver(), this);
 
      }
 
