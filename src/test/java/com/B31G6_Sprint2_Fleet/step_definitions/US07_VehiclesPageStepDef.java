2ddfe08f26b915e3cb24db979a0da7dc27e82655
oliveradzuver <dzuvero@gmail.com>
2024-01-14 16:59:58 +0100

    changes

---
+++ src/test/java/com/B31G6_Sprint2_Fleet/step_definitions/US07_VehiclesPageStepDef.java
@@ -0,0 +1,57 @@
+package com.B31G6_Sprint2_Fleet.step_definitions;
+
+
+import com.B31G6_Sprint2_Fleet.pages.HomePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.remote.http.Contents;
+
+public class US07_VehiclesPageStepDef extends HomePage {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+
+    }
+    @When("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+
+    }
+    @When("the user navigates to {string} to {string}")
+    public void the_user_navigates_to_to(String string, String string2) {
+
+    }
+    @When("the user select the all cars")
+    public void the_user_select_the_all_cars() {
+
+    }
+
+    @When("the user select any car {int}")
+    public void the_user_select_any_car(Integer int1) {
+
+    }
+
+    @Then("the user should be able to see all the checkboxes as {string}")
+    public void the_user_should_be_able_to_see_all_the_checkboxes_as(String string) {
+
+    }
+
+
+    @Then("the user should be able to see car as {string}")
+    public void the_user_should_be_able_to_see_car_as(String string) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
